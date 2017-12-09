631d91a6cbe74bdbfba08d1d7644871c90e356e6
Yon <valandil.yon@gmail.com>
2017-12-09 11:14:18 +0100

    test calculs zoom

--- src/event.c
+++ src/event.c
@@ -22,14 +22,17 @@ int mouse_zoom(int keycode, int x, int y, t_env *env)
 	{
 		if (keycode == ZOOM_IN || keycode == 5)
 		{
-			env->fra.x1 += x - WIDTH_IMG;
-			env->fra.y1 += y - HEIGHT_IMG;
 			env->fra.zoom *= 1.1;
+
+			env->fra.x2 = (env->fra.x2 - x ) / env->fra.zoom;
+			env->fra.x1 = (env->fra.x1 - x) / 1.1;
+			env->fra.y2 = (env->fra.y2 - y) / env->fra.zoom;
+			env->fra.y1 = (env->fra.y1 - y) / env->fra.zoom;
 		}
 		if (keycode == ZOOM_OUT || keycode == 4)
 		{
-			env->fra.x1 -= ;
-			env->fra.y1 -= ;
+			// env->fra.x1 -= ;
+			// env->fra.y1 -= ;
 			env->fra.zoom /= 1.1;
 		}
 		set_img(env);
